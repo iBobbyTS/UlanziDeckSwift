08218643ba21977d6bc3d39d956d9257658d4404
iBobbyTS <ibobbyts@gmail.com>
2026-06-13 09:36:57 -0600

    Initial Commit

---
+++ UlanziDeckSwiftTests/UlanziDeckSwiftTests.swift
@@ -0,0 +1,19 @@
+//
+//  UlanziDeckSwiftTests.swift
+//  UlanziDeckSwiftTests
+//
+//  Created by iBobby on 2026-06-13.
+//
+
+import Testing
+@testable import UlanziDeckSwift
+
+struct UlanziDeckSwiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
